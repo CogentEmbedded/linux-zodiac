d7439b2f32d53d440283ceda3b151e6fa0578ee7
Philipp Zabel <p.zabel@pengutronix.de>
2018-06-14 12:55:13 +0200

    gpu: ipu-v3: image-convert: calculate tile dimensions and offsets outside fill_image
    This will allow to calculate seam positions after initializing the
    ipu_image base structure but before calculating tile dimensions.
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/gpu/ipu-v3/ipu-image-convert.c
+++ drivers/gpu/ipu-v3/ipu-image-convert.c
@@ -1453,9 +1453,6 @@ static int fill_image(struct ipu_image_convert_ctx *ctx,
 	else
 		ic_image->stride  = ic_image->base.pix.bytesperline;
 
-	calc_tile_dimensions(ctx, ic_image);
-	calc_tile_offsets(ctx, ic_image);
-
 	return 0;
 }
 
@@ -1660,16 +1657,22 @@ ipu_image_convert_prepare(struct ipu_soc *ipu, enum ipu_ic_task ic_task,
 	ctx->num_tiles = d_image->num_cols * d_image->num_rows;
 	ctx->rot_mode = rot_mode;
 
-	ret = calc_image_resize_coefficients(ctx, in, out);
-	if (ret)
-		goto out_free;
-
 	ret = fill_image(ctx, s_image, in, IMAGE_CONVERT_IN);
 	if (ret)
 		goto out_free;
 	ret = fill_image(ctx, d_image, out, IMAGE_CONVERT_OUT);
 	if (ret)
 		goto out_free;
+
+	ret = calc_image_resize_coefficients(ctx, in, out);
+	if (ret)
+		goto out_free;
+
+	calc_tile_dimensions(ctx, s_image);
+	calc_tile_offsets(ctx, s_image);
+
+	calc_tile_dimensions(ctx, d_image);
+	calc_tile_offsets(ctx, d_image);
 
 	calc_out_tile_map(ctx);
 	calc_tile_resize_coefficients(ctx);
