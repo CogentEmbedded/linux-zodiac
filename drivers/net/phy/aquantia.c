ec99f42e117f01a2915fe599c9aaf7056001232d
Andrew Lunn <andrew@lunn.ch>
2018-06-22 20:25:18 +0200

    net: phy: aquantia: Add hwmon support for temperature sensor
    This PHY has an embedded temperature sensors, with lcrit/min/max/crit
    alarms. Add hwmon support.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>

--- drivers/net/phy/aquantia.c
+++ drivers/net/phy/aquantia.c
@@ -12,9 +12,11 @@
 
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/ctype.h>
 #include <linux/delay.h>
 #include <linux/mii.h>
 #include <linux/ethtool.h>
+#include <linux/hwmon.h>
 #include <linux/phy.h>
 #include <linux/mdio.h>
 
@@ -29,6 +31,25 @@
 				 SUPPORTED_1000baseT_Full | \
 				 SUPPORTED_100baseT_Full | \
 				 PHY_DEFAULT_FEATURES)
+
+/* Vendor specific 2, MMIO_MMD_VEND2 */
+#define VEND2_THERMAL_PROV_HIGH_TEMP_FAIL	0xc421
+#define VEND2_THERMAL_PROV_LOW_TEMP_FAIL	0xc422
+#define VEND2_THERMAL_PROV_HIGH_TEMP_WARN	0xc423
+#define VEND2_THERMAL_PROV_LOW_TEMP_WARN	0xc424
+#define VEND2_THERMAL_STAT1			0xc820
+#define VEND2_THERMAL_STAT2			0xc821
+#define VEND2_THERMAL_STAT2_VALID		BIT(0)
+#define VEND2_GENERAL_STAT1			0xc830
+#define VEND2_GENERAL_STAT1_HIGH_TEMP_FAIL	BIT(0xe)
+#define VEND2_GENERAL_STAT1_LOW_TEMP_FAIL	BIT(0xd)
+#define VEND2_GENERAL_STAT1_HIGH_TEMP_WARN	BIT(0xc)
+#define VEND2_GENERAL_STAT1_LOW_TEMP_WARN	BIT(0xb)
+
+struct aqr_priv {
+	struct device *hwmon_dev;
+	char *hwmon_name;
+};
 
 static int aqr_config_aneg(struct phy_device *phydev)
 {
@@ -110,6 +131,218 @@ static int aqr_read_status(struct phy_device *phydev)
 	return 0;
 }
 
+#ifdef CONFIG_HWMON
+static umode_t aqr_hwmon_is_visible(const void *data,
+				    enum hwmon_sensor_types type,
+				    u32 attr, int channel)
+{
+	if (type != hwmon_temp)
+		return 0;
+
+	switch (attr) {
+	case hwmon_temp_input:
+	case hwmon_temp_min_alarm:
+	case hwmon_temp_max_alarm:
+	case hwmon_temp_lcrit_alarm:
+	case hwmon_temp_crit_alarm:
+	case hwmon_temp_min:
+	case hwmon_temp_max:
+	case hwmon_temp_lcrit:
+	case hwmon_temp_crit:
+		return 0444;
+	default:
+		return 0;
+	}
+}
+
+static int aqr_hwmon_get(struct phy_device *phydev, int reg, long *value)
+{
+	int temp = phy_read_mmd(phydev, MDIO_MMD_VEND2, reg);
+
+	if (temp < 0)
+		return temp;
+
+	temp = ((temp & 0xff) - 75) * 1000;
+
+	*value = temp;
+
+	return 0;
+}
+
+static int aqr_hwmon_status1(struct phy_device *phydev, int bit, long *value)
+{
+	int reg = phy_read_mmd(phydev, MDIO_MMD_VEND2, VEND2_GENERAL_STAT1);
+
+	if (reg < 0)
+		return reg;
+
+	*value = !!(reg & bit);
+
+	return 0;
+}
+
+static int aqr_hwmon_read(struct device *dev, enum hwmon_sensor_types type,
+			  u32 attr, int channel, long *value)
+{
+	struct phy_device *phydev = dev_get_drvdata(dev);
+	int reg;
+
+	if (type != hwmon_temp)
+		return -EOPNOTSUPP;
+
+	switch (attr) {
+	case hwmon_temp_input:
+		reg = phy_read_mmd(phydev, MDIO_MMD_VEND2,
+				   VEND2_THERMAL_STAT2);
+		if (reg < 0)
+			return reg;
+		if (!(reg & VEND2_THERMAL_STAT2_VALID))
+			return -EIO;
+
+		return aqr_hwmon_get(phydev, VEND2_THERMAL_STAT1, value);
+
+	case hwmon_temp_lcrit:
+		return aqr_hwmon_get(phydev, VEND2_THERMAL_PROV_LOW_TEMP_FAIL,
+				     value);
+
+	case hwmon_temp_min:
+		return aqr_hwmon_get(phydev, VEND2_THERMAL_PROV_LOW_TEMP_WARN,
+				     value);
+
+	case hwmon_temp_max:
+		return aqr_hwmon_get(phydev, VEND2_THERMAL_PROV_HIGH_TEMP_WARN,
+				     value);
+
+	case hwmon_temp_crit:
+		return aqr_hwmon_get(phydev, VEND2_THERMAL_PROV_HIGH_TEMP_FAIL,
+				     value);
+
+	case hwmon_temp_lcrit_alarm:
+		return aqr_hwmon_status1(phydev,
+					 VEND2_GENERAL_STAT1_LOW_TEMP_FAIL,
+					 value);
+
+	case hwmon_temp_min_alarm:
+		return aqr_hwmon_status1(phydev,
+					 VEND2_GENERAL_STAT1_LOW_TEMP_WARN,
+					 value);
+
+	case hwmon_temp_max_alarm:
+		return aqr_hwmon_status1(phydev,
+					 VEND2_GENERAL_STAT1_HIGH_TEMP_WARN,
+					 value);
+
+	case hwmon_temp_crit_alarm:
+		return aqr_hwmon_status1(phydev,
+					 VEND2_GENERAL_STAT1_HIGH_TEMP_FAIL,
+					 value);
+	default:
+		return -EOPNOTSUPP;
+	}
+
+	return -EOPNOTSUPP;
+}
+
+static const struct hwmon_ops aqr_hwmon_ops = {
+	.is_visible = aqr_hwmon_is_visible,
+	.read = aqr_hwmon_read,
+};
+
+static u32 aqr_hwmon_chip_config[] = {
+	HWMON_C_REGISTER_TZ,
+	0,
+};
+
+static const struct hwmon_channel_info aqr_hwmon_chip = {
+	.type = hwmon_chip,
+	.config = aqr_hwmon_chip_config,
+};
+
+static u32 aqr_hwmon_temp_config[] = {
+	HWMON_T_INPUT |
+	HWMON_T_MAX | HWMON_T_MIN |
+	HWMON_T_MAX_ALARM | HWMON_T_MIN_ALARM |
+	HWMON_T_CRIT | HWMON_T_LCRIT,
+	HWMON_T_CRIT_ALARM | HWMON_T_LCRIT_ALARM,
+	0,
+};
+
+static const struct hwmon_channel_info aqr_hwmon_temp = {
+	.type = hwmon_temp,
+	.config = aqr_hwmon_temp_config,
+};
+
+static const struct hwmon_channel_info *aqr_hwmon_info[] = {
+	&aqr_hwmon_chip,
+	&aqr_hwmon_temp,
+	NULL,
+};
+
+static const struct hwmon_chip_info aqr_hwmon_chip_info = {
+	.ops = &aqr_hwmon_ops,
+	.info = aqr_hwmon_info,
+};
+
+static int aqr_hwmon_probe(struct phy_device *phydev)
+{
+	struct device *dev = &phydev->mdio.dev;
+	struct aqr_priv *priv = dev_get_drvdata(&phydev->mdio.dev);
+	int i, j;
+
+	priv->hwmon_name = devm_kstrdup(dev, dev_name(dev), GFP_KERNEL);
+	if (!priv->hwmon_name)
+		return -ENODEV;
+
+	for (i = j = 0; priv->hwmon_name[i]; i++) {
+		if (isalnum(priv->hwmon_name[i])) {
+			if (i != j)
+				priv->hwmon_name[j] = priv->hwmon_name[i];
+			j++;
+		}
+	}
+	priv->hwmon_name[j] = '\0';
+
+	priv->hwmon_dev = devm_hwmon_device_register_with_info(dev,
+				priv->hwmon_name, phydev,
+				&aqr_hwmon_chip_info, NULL);
+
+	return PTR_ERR_OR_ZERO(priv->hwmon_dev);
+}
+#else
+static inline int aqr_hwmon_config(struct phy_device *phydev, bool enable)
+{
+	return 0;
+}
+
+static int aqr_hwmon_probe(struct phy_device *phydev)
+{
+	return 0;
+}
+#endif
+
+static int aqr_probe(struct phy_device *phydev)
+{
+	struct aqr_priv *priv;
+	u32 mmd_mask = MDIO_DEVS_PMAPMD | MDIO_DEVS_AN;
+	int ret;
+
+	if (!phydev->is_c45 ||
+	    (phydev->c45_ids.devices_in_package & mmd_mask) != mmd_mask)
+		return -ENODEV;
+
+	priv = devm_kzalloc(&phydev->mdio.dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+
+	dev_set_drvdata(&phydev->mdio.dev, priv);
+
+	ret = aqr_hwmon_probe(phydev);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
 static struct phy_driver aqr_driver[] = {
 {
 	.phy_id		= PHY_ID_AQ1202,
@@ -165,6 +398,7 @@ static struct phy_driver aqr_driver[] = {
 	.name		= "Aquantia AQR107",
 	.features	= PHY_AQR_FEATURES,
 	.flags		= PHY_HAS_INTERRUPT,
+	.probe          = aqr_probe,
 	.aneg_done	= genphy_c45_aneg_done,
 	.config_aneg    = aqr_config_aneg,
 	.config_intr	= aqr_config_intr,
